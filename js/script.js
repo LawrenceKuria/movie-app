52225a41827f718af9734f0d7f5ba656bc0c2eec
Lawrence Kuria <i3uyer2@gmail.com>
2025-07-22 09:21:03 -0700

    Fixed issue with displaying tv shows

--- js/script.js
+++ js/script.js
@@ -661,6 +661,7 @@ function init() {
             displayPopularMovies()
             break
         case '/shows.html':
+        case '/shows':
             displayPopularTVShows()
             break
         case '/movie-details.html':
